ed0382488cfecc6ed572f7e4d5b5149c8d7bb72e
doujia <1528439908@qq.com>
2020-09-22 20:06:26 +0800

    再次测试webhook

---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './index.less';
+const http = require("http")
+
+export default () => {
+http.createServer((req, res) => {
+    console.log('receive request')
+    console.log(req.url)
+    if (req.method === 'POST' && req.url === '/') {
+        //...
+    }
+    res.end('ok')
+}).listen(8080,()=>{
+    console.log('server is ready')
+})
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
