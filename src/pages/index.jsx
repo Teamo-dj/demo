60c8bc5cc2042fb45913bb432e432801577f9cbf
doujia <1528439908@qq.com>
2020-09-22 20:35:02 +0800

    测试webhook

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import styles from './index.less';
-const http = require("http")
 
 export default () => {
-http.createServer((req, res) => {
-    console.log('receive request')
-    console.log(req.url)
-    if (req.method === 'POST' && req.url === '/') {
-        //...
-    }
-    res.end('ok')
-}).listen(3000,()=>{
-    console.log('server is ready')
-})
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
