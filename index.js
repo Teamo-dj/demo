6f32ebda05600578f220a492f515529903e85a32
doujia <1528439908@qq.com>
2020-09-23 15:21:09 +0800

    去

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require("http")
-const {execSync} = require("child_process")
+const { execSync } = require("child_process")
 const fs = require("fs")
 const path = require("path")
 
 // 递归删除目录
 function deleteFolderRecursive(path) {
-    if( fs.existsSync(path) ) {
-        fs.readdirSync(path).forEach(function(file) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file) {
             const curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) { // recurse
+            if (fs.statSync(curPath).isDirectory()) { // recurse
                 deleteFolderRecursive(curPath);
             } else { // delete file
                 fs.unlinkSync(curPath);
@@ -33,39 +33,42 @@ http.createServer(async (req, res) => {
     console.log('receive request')
     console.log(req.url)
     if (req.method === 'POST' && req.url === '/') {
-      const data = await resolvePost(req);
-      const projectDir = path.resolve(`./${data.repository.name}`)
-     deleteFolderRecursive(projectDir)
-      // 拉取仓库最新代码
-      execSync(`git clone https://github.com/Teamo-dj/${data.repository.name}.git ${projectDir}`,{
-        stdio:'inherit',
-    })
-    
-    // 复制 Dockerfile 到项目目录
-     fs.copyFileSync(path.resolve(`./Dockerfile`), path.resolve(projectDir,'./Dockerfile'))
+        const data = await resolvePost(req);
+        const projectDir = path.resolve(`./${data.repository.name}`)
+        console.log(projectDir)
+         deleteFolderRecursive(projectDir)
+         console.log("删除完毕")
+        // 拉取仓库最新代码
+        execSync(`git clone https://github.com/Teamo-dj/${data.repository.name}.git ${projectDir}`, {
+            stdio: 'inherit',
+        })
+        console.log("开始复制")
+        // 复制 Dockerfile 到项目目录
+        fs.copyFileSync(path.resolve(`./Dockerfile`), path.resolve(projectDir, './Dockerfile'))
 
-     // 复制 .dockerignore 到项目目录
-    fs.copyFileSync(path.resolve(__dirname,`./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
+        // 复制 .dockerignore 到项目目录
+        fs.copyFileSync(path.resolve(__dirname, `./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
 
-      // 创建 docker 镜像
-     execSync(`docker build . -t ${data.repository.name}-image:latest `,{
-      stdio:'inherit',
-      cwd: projectDir
-   })
+        // 创建 docker 镜像
+        console.log(`创建docker镜像:${data.repository.name}`)
+        execSync(`docker build . -t ${data.repository.name}-image:latest `, {
+            stdio: 'inherit',
+            cwd: projectDir
+        })
 
-      // 销毁 docker 容器
-     execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
-      stdio: 'inherit',
-  })
+        // 销毁 docker 容器
+        execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
+            stdio: 'inherit',
+        })
 
-     // 创建 docker 容器
-     execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container  ${data.repository.name}-image:latest`, {
-      stdio:'inherit',
-  })
+        // 创建 docker 容器
+        execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container  ${data.repository.name}-image:latest`, {
+            stdio: 'inherit',
+        })
 
-   console.log('deploy success')
-    res.end('ok')
-}
+        console.log('deploy success')
+        res.end('ok')
+    }
 }).listen(3000, () => {
     console.log('server is ready')
 })
