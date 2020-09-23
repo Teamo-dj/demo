7ec176af47d77a5e07e1aa907cb12ab8e5f62cf9
doujia <1528439908@qq.com>
2020-09-23 11:02:30 +0800

    测试webhook

---
+++ index.js
@@ -0,0 +1,48 @@
+ const http = require("http")
+ const {execSync} = require("child_process")
+ const path = require("path")
+const fs = require("fs")
+
+ // 递归删除目录
+ function deleteFolderRecursive(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file) {
+            const curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+ }
+
+ const resolvePost = req =>
+    new Promise(resolve => {
+     let chunk = "";
+        req.on("data", data => {
+            chunk += data;
+        });
+        req.on("end", () => {
+         resolve(JSON.parse(chunk));
+     });
+    });
+
+http.createServer(async (req, res) => {
+    console.log('receive request')
+    console.log(req.url)
+    if (req.method === 'POST' && req.url === '/') {
+     const data = await resolvePost(req);
+     const projectDir = path.resolve(`./${data.repository.name}`)
+     deleteFolderRecursive(projectDir)
+
+  // 拉取仓库最新代码
+    execSync(`git clone https://github.com/yeyan1996/${data.repository.name}.git ${projectDir}`,{
+       stdio:'inherit',
+   })
+}
+    res.end('ok')
+}).listen(3000, () => {
+    console.log('server is ready')
+})
