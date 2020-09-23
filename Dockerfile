a070b3b8ac09062c50bc03cbb23c4ffe557c60c6
doujia <1528439908@qq.com>
2020-09-23 13:14:30 +0800

    hj

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,3 @@ COPY --from=build-stage /app/dist /usr/share/nginx/html
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
 
-
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,10 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>
+        Page index dddjkkll
+      </h1>
     </div>
   );
 }
+
