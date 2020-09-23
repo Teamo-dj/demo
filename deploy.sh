b4e388bf517ba8ab41d1a6f619c5507c7dfae148
doujia <1528439908@qq.com>
2020-09-23 18:10:21 +0800

    12

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,12 @@ echo "----------git pull----------"
 git pull
 echo "----------docker build----------"
 docker build . -t unimas-image:latest 
+echo "----------docker container delete----------"
+# docker ps -a -f name=unimas-container | xargs -r docker stop | xargs -r docker rm
+echo "----------docker container stop----------"
+docker stop unimas-container 
+echo "----------docker container delete----------"
+docker rm unimas-container
 echo "----------docker container create----------"
 docker run -d -p 8888:80 --name unimas-container  unimas-image:latest
 echo "deploy success"
